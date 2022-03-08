8968763d51ae211e846b9e252fd94eefafae5910
JingBh <jingbohao@yeah.net>
2022-03-08 20:58:53 +0800

    chore: remove useless code

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,14 +1,5 @@
 // app.ts
 
-App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
-  },
+App({
+  globalData: {}
 })
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,3 @@
-interface IAppOption {
-  globalData: {
-    userInfo?: WechatMiniprogram.UserInfo,
-  }
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
-}
-
 interface StringEvent extends WechatMiniprogram.BaseEvent {
   detail: string
 }
