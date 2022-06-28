cfae0147708a915149635722493c6c683cde4d86
JingBh <jingbohao@yeah.net>
2022-06-28 12:47:23 +0800

    chore: update countdown

--- miniprogram/utils/countdown.ts
+++ miniprogram/utils/countdown.ts
@@ -1,5 +1,5 @@
-const gaokaoDate = 1654531200000 // 2022-06-07
-const zhongkaoDate = 1656000000000 // 2022-06-24
+const gaokaoDate = 1686067200000 // 2023-06-07
+const zhongkaoDate = 1687536000000 // 2023-06-24
 const day = 1000 * 60 * 60 * 24
 
 export const getDaysToGaokao = (time: number = new Date().getTime()) => Math.ceil((gaokaoDate - time) / day)
--- test/countdown.test.ts
+++ test/countdown.test.ts
@@ -1,21 +1,21 @@
 import { getDaysToGaokao } from '../miniprogram/utils/countdown'
 
 test('countdown before', () => {
-  const time = 1654041600000
+  const time = 1685577600000
   expect(getDaysToGaokao(time)).toBeGreaterThan(0)
 })
 
 test('countdown first day', () => {
-  const time = 1654560000000
+  const time = 1686096000000
   expect(getDaysToGaokao(time)).toBeLessThanOrEqual(0)
 })
 
 test('countdown last day', () => {
-  const time = 1654819200000
+  const time = 1686355200000
   expect(getDaysToGaokao(time)).toBeGreaterThanOrEqual(-3)
 })
 
 test('countdown after', () => {
-  const time = 1655164800000
+  const time = 1686700800000
   expect(getDaysToGaokao(time)).toBeLessThan(-3)
 })
