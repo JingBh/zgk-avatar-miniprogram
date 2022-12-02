23779c7d4f793a879e4c4d0f80156685459cb25f
JingBh <jingbohao@yeah.net>
2022-12-02 20:07:54 +0800

    feat: use native cropping when supported
    reference: https://developers.weixin.qq.com/miniprogram/dev/api/media/image/wx.editImage.html
    
    release: v1.2.4

--- miniprogram/pages/select_image/cropper.ts
+++ miniprogram/pages/select_image/cropper.ts
@@ -1,10 +1,12 @@
 // pages/select_image/cropper.ts
+import compareVersion from '../../utils/compare-version'
 import { shareMsg, shareTimeline } from '../../utils/share'
 
 const { saveFile, unlink } = wx.getFileSystemManager()
 
 Page({
   data: {
+    hasNative: true,
     imageSrc: ''
   },
 
@@ -13,12 +15,33 @@ Page({
       wx.navigateBack()
       return
     }
+    const imageSrc = decodeURIComponent(query.src)
+
     wx.showLoading({
       title: '加载图片中',
       mask: true
     })
+
+    // check for native cropping
+    if (wx.cropImage && compareVersion(wx.getSystemInfoSync().SDKVersion, '2.28.1') >= 0) {
+      wx.cropImage({
+        src: imageSrc,
+        cropScale: '1:1',
+        success: ({ tempFilePath }) => this.saveImage(tempFilePath),
+        fail: (error) => {
+          console.error(error)
+          wx.navigateBack()
+        },
+        complete: () => wx.hideLoading()
+      })
+    } else {
+      this.setData({
+        hasNative: false
+      })
+    }
+
     this.setData({
-      imageSrc: decodeURIComponent(query.src)
+      imageSrc
     })
   },
 
@@ -27,11 +50,12 @@ Page({
     wx.hideLoading()
   },
 
-  saveImage() {
+  onCropFinish() {
     wx.showLoading({
       title: '处理图片中',
       mask: true
     })
+
     const cropper = this.selectComponent("#cropper")
     cropper.setData({
       export_scale: 1024 / cropper.data.width
@@ -40,52 +64,54 @@ Page({
       url: string,
       width: number,
       height: number
-    }) => {
-      // delete old image
-      wx.getStorage({
-        key: 'background',
-        success: ({ data }) => {
-          if (data.startsWith('fs:')) {
-            unlink({
-              filePath: `${wx.env.USER_DATA_PATH}/${data.substring(3)}`
+    }) => this.saveImage(result.url))
+  },
+
+  saveImage(url: string) {
+    // delete old image
+    wx.getStorage({
+      key: 'background',
+      success: ({ data }) => {
+        if (data.startsWith('fs:')) {
+          unlink({
+            filePath: `${wx.env.USER_DATA_PATH}/${data.substring(3)}`
+          })
+        }
+      }
+    })
+
+    // save new image
+    const filename = `background${Date.now()}.png`
+    saveFile({
+      tempFilePath: url,
+      filePath: `${wx.env.USER_DATA_PATH}/${filename}`,
+      success: () => {
+        wx.setStorage({
+          key: 'background',
+          data: `fs:${filename}`,
+          success: () => {
+            wx.redirectTo({
+              url: '/pages/select_image/foreground'
+            })
+          },
+          fail: () => {
+            wx.hideLoading()
+            wx.showToast({
+              title: '保存图片失败',
+              icon: 'error',
+              duration: 2000
             })
           }
-        }
-      })
-
-      // save new image
-      const filename = `background${Date.now()}.png`
-      saveFile({
-        tempFilePath: result.url,
-        filePath: `${wx.env.USER_DATA_PATH}/${filename}`,
-        success: () => {
-          wx.setStorage({
-            key: 'background',
-            data: `fs:${filename}`,
-            success: () => {
-              wx.redirectTo({
-                url: '/pages/select_image/foreground'
-              })
-            },
-            fail: () => {
-              wx.hideLoading()
-              wx.showToast({
-                title: '保存图片失败',
-                icon: 'error',
-                duration: 2000
-              })
-            }
-          })
-        },
-        fail: () => {
-          wx.hideLoading()
-          wx.showToast({
-            title: '保存图片失败',
-            icon: 'error',
-            duration: 2000
-          })
-        }
-      })
+        })
+      },
+      fail: () => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '保存图片失败',
+          icon: 'error',
+          duration: 2000
+        })
+      }
     })
   },
 
